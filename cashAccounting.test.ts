a583c6e10bc41d1aef19960cf7ac343bd6a2e266
ik0v <ivan.kovals@hotmail.com>
2024-11-04 12:06:39 +0100

    functionality to count coin as well (by amount or grams) is implemented. Corresponding tests are passing

--- cashAccounting.test.ts
+++ cashAccounting.test.ts
@@ -13,3 +13,25 @@ test("test summing bills of 100kr", () => {
 test("test summing bills of different types", () => {
   expect(sumBalance({ "1000kr": 1, "200kr": 2, "100kr": 5 })).toBe(1900);
 });
+
+test("test summing coins of different types", () => {
+  expect(sumBalance({ "20kr": { count: 3 }, "1kr": { count: 3 } })).toBe(63);
+});
+
+test("test summing coins by weight", () => {
+  expect(sumBalance({ "20kr": { grams: 19.8 } })).toBe(40);
+});
+
+test("test summing coins and bills", () => {
+  expect(
+    sumBalance({
+      "100kr": 2,
+      "20kr": { grams: 19.8 },
+      "10kr": { count: 4 },
+    }),
+  ).toBe(280);
+});
+
+test("test counting coins be weight, integer numbers", () => {
+  expect(sumBalance({ "20kr": { grams: 20 } })).toBe(40);
+});
--- sumBalance.ts
+++ sumBalance.ts
@@ -9,19 +9,35 @@ const BillTypeValues = [
 type BillType = (typeof BillTypeValues)[number];
 type BillTypeDenomination = BillType["key"];
 
-type CashSettlement = Partial<Record<BillTypeDenomination, number>>;
+interface CoinAmount {
+  count?: number;
+  grams?: number;
+}
 
-const denomination1: BillTypeDenomination = "100kr";
+const CoinTypeValues = [
+  { key: "20kr", value: 20, gramsPerCoin: 9.9 },
+  { key: "10kr", value: 10, gramsPerCoin: 6.8 },
+  { key: "5kr", value: 5, gramsPerCoin: 7.85 },
+  { key: "1kr", value: 1, gramsPerCoin: 4.35 },
+] as const;
+
+type CoinType = (typeof CoinTypeValues)[number];
+type CoinTypeDenomination = CoinType["key"];
+
+type CashSettlement = Partial<Record<BillTypeDenomination, number>> &
+  Partial<Record<CoinTypeDenomination, CoinAmount>>;
 
 export function sumBalance(cashSettlement: CashSettlement): number {
   let result = 0;
   for (const { key, value } of BillTypeValues) {
     result += (cashSettlement[key] || 0) * value;
   }
-  /*    for (const type of BillTypeValues) {
-        if(cashSettlement[type.key]) {
-            result += cashSettlement[type.key] * type.value;
-        }
-    }*/
+  for (const { key, value, gramsPerCoin } of CoinTypeValues) {
+    if (cashSettlement[key]) {
+      result +=
+        (cashSettlement[key]?.count || 0) * value +
+        Math.round((cashSettlement[key]?.grams || 0) / gramsPerCoin) * value;
+    }
+  }
   return result;
 }
